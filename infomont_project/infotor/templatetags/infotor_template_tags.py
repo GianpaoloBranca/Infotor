d46d59aa2d3043781551eb5006905c31e37e0ecb
Gianpaolo Branca <gianpaolo@protonmail.com>
2018-04-16 15:13:58 +0200

    Handle max length with template tags

--- infomont_project/infotor/templatetags/infotor_template_tags.py
+++ infomont_project/infotor/templatetags/infotor_template_tags.py
@@ -5,3 +5,7 @@ register = template.Library()
 @register.simple_tag
 def verbose_name(obj, field_name):
     return obj._meta.get_field(field_name).verbose_name
+
+@register.simple_tag
+def max_length(obj, field_name):
+    return obj._meta.get_field(field_name).max_length
--- infomont_project/infotor/views.py
+++ infomont_project/infotor/views.py
@@ -19,35 +19,13 @@ def show_torrent(request, torrent_id):
     data = Torrente.objects.get(IDtrat=torrent_id)
     data_dict = model_to_dict(data)
 
-    max_length = get_max_fields_length(Torrente)
-    #data_with_length = add_max_length_to_items(max_length, data_dict)
     # non voglio che la chiave primaria venga mandata alla pagina
-    #del data_with_length['IDtrat']
+    del data_dict['IDtrat']
 
     context_dict = {
         'torrents': torrents,
         'torrente': data,
         'data_dict': data_dict,
-        'field_max_length': max_length
     }
 
     return render(request, 'infotor/torrenti.html', context_dict)
-
-def get_max_fields_length(Model):
-
-    max_lengths = []
-    fields = Model._meta.get_fields() #tuple
-
-    for field in fields:
-        max_lengths.append(field.max_length)
-
-    return max_lengths
-
-def add_max_length_to_items(max_lengths, data_dict):
-
-    output = {}
-
-    for i, (key, value) in enumerate(data_dict.items()):
-        output[key] = (value, max_lengths[i])
-
-    return output
